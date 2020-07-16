67a3e525babf7e1155b27ae2da261da88d5faeaa
Daniel Gyorffy <judalion777@gmail.com>
2020-07-16 14:09:23 +0200

    Selection toolbaritem in DBManagerView

--- src/main/java/com/dansoftware/libraryapp/gui/entry/login/dbmanager/DBManagerView.java
+++ src/main/java/com/dansoftware/libraryapp/gui/entry/login/dbmanager/DBManagerView.java
@@ -2,9 +2,14 @@ package com.dansoftware.libraryapp.gui.entry.login.dbmanager;
 
 import com.dansoftware.libraryapp.db.DBMeta;
 import com.dansoftware.libraryapp.gui.workbench.SimpleHeaderView;
+import com.dansoftware.libraryapp.locale.I18N;
 import com.dlsc.workbenchfx.view.controls.ToolbarItem;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.value.ObservableValue;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -30,6 +35,23 @@ public class DBManagerView extends SimpleHeaderView<DBManagerTable> {
     public DBManagerView(@NotNull List<DBMeta> items) {
         super(getGeneralWord("database.manager.title"), new MaterialDesignIconView(MaterialDesignIcon.DATABASE));
         super.setContent(new DBManagerTable(this, items));
+        this.createToolbarControls();
+    }
+
+    private void createToolbarControls() {
+        //
+        ToolbarItem selectedItemsIndicator = new ToolbarItem();
+        IntegerBinding fullItemsProperty = Bindings.size(this.getContent().getItems());
+        IntegerBinding selectedItemsProperty = Bindings.size(this.getContent().getSelectionModel().getSelectedItems());
+        ObservableValue<String> allItemsSlashSelected = fullItemsProperty.asString()
+                .concat("/")
+                .concat(selectedItemsProperty)
+                .concat(StringUtils.SPACE)
+                .concat(I18N.getGeneralWord("database.manager.selected"));
+        selectedItemsIndicator.textProperty().bind(allItemsSlashSelected);
+        this.getToolbarControlsRight().add(selectedItemsIndicator);
+
+        //
         this.getToolbarControlsRight().add(new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.RELOAD), event -> {
             this.getContent().refresh();
         }));
