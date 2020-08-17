37d0eca1a32b68aa097f06850bc370f30d5da359
Daniel Gyorffy <judalion777@gmail.com>
2020-08-17 19:20:40 +0200

    Fixes and some doc

--- src/main/java/com/dansoftware/libraryapp/main/InstanceService.java
+++ src/main/java/com/dansoftware/libraryapp/main/InstanceService.java
@@ -3,25 +3,35 @@ package com.dansoftware.libraryapp.main;
 import it.sauronsoftware.junique.AlreadyLockedException;
 import it.sauronsoftware.junique.JUnique;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.stream.Stream;
-
+/**
+ * InstanceService is responsible for listening to application instances
+ * and avoiding them if there is an already running instance of the application.
+ * It also forwards the application arguments to the already running instance.
+ *
+ * <p>
+ * The {@link #open(String[])} method should be used for starting the 'service', and it
+ * should be called immediately after the program starts.
+ *
+ * @author Daniel Gyorffy
+ */
 public class InstanceService {
 
     private static final Logger logger = LoggerFactory.getLogger(InstanceService.class);
 
     private static final String APPLICATION_ID = "com.dansoftware.libraryapp";
 
-    private static final String FOCUS = "focus";
-
     private static InstanceService instance;
 
     private InstanceService(String[] args) {
         try {
             JUnique.acquireLock(APPLICATION_ID, arg -> {
-                if (FOCUS.equals(arg)) {
+                logger.debug("Message from another process: {}", arg);
+
+                if (StringUtils.isEmpty(arg)) {
                     //focusing on some window
                 } else {
                     //if the file that the argument represents is already opened
@@ -35,16 +45,15 @@ public class InstanceService {
             logger.info("Sending the arguments to the already running instance...");
 
             if (ArrayUtils.isEmpty(args)) {
-                JUnique.sendMessage(APPLICATION_ID, FOCUS);
+                JUnique.sendMessage(APPLICATION_ID, StringUtils.EMPTY);
             } else {
                 JUnique.sendMessage(APPLICATION_ID, args[0]);
             }
 
             logger.info("Exiting...");
-            System.exit(1);
+            System.exit(0);
         }
     }
-
 
     public static void open(String[] args) {
         if (instance == null) {
