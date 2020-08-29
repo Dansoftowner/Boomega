59e1c165ea94981fc215257430ae14074bbacec8
Daniel Gyorffy <judalion777@gmail.com>
2020-08-29 16:21:34 +0200

    cleaning in the plugin package

--- src/main/java/com/dansoftware/libraryapp/plugin/PluginClassLoader.java
+++ src/main/java/com/dansoftware/libraryapp/plugin/PluginClassLoader.java
@@ -1,30 +1,19 @@
 package com.dansoftware.libraryapp.plugin;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.IOException;
-import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.regex.Pattern;
 
+/**
+ * The {@link PluginClassLoader} can load classes from both the class-path and the third-party plugins located
+ * in the plugin-directory.
+ *
+ * @author Daniel Gyorffy
+ */
 public class PluginClassLoader extends URLClassLoader {
 
     private static final PluginClassLoader instance = new PluginClassLoader();
 
     private PluginClassLoader() {
-        super(getSources(), ClassLoader.getSystemClassLoader());
-    }
-
-    private static URL[] getSources() {
-        Pattern pattern = Pattern.compile(".*\\.jar");
-
-        try {
-            return FileUtils.toURLs(
-                    PluginDirectory.getDirectory().listFiles((dir, name) -> pattern.matcher(name).matches())
-            );
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        super(new PluginDirectory().getPluginFileURLS(), ClassLoader.getSystemClassLoader());
     }
 
     public static PluginClassLoader getInstance() {
--- src/main/java/com/dansoftware/libraryapp/plugin/PluginDirectory.java
+++ src/main/java/com/dansoftware/libraryapp/plugin/PluginDirectory.java
@@ -1,32 +1,64 @@
 package com.dansoftware.libraryapp.plugin;
 
 import com.sun.javafx.PlatformUtil;
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Pattern;
 
-public class PluginDirectory {
+/**
+ * Represents the directory that holds the plugin jar files on the disk.
+ *
+ * @author Daniel Gyorffy
+ */
+public class PluginDirectory extends File {
 
-    private static final String DIRECTORY_NAME = "plugins";
+    private static final String DIRECTORY_NAME = "plugin";
 
-    public static File getDirectory() {
+    private static final String PATH;
+
+    static {
         if (PlatformUtil.isWindows()) {
-            return new File(String.join(
-                    File.separator, System.getenv("APPDATA"), "Dansoftware", "libraryapp_2020", DIRECTORY_NAME
-            ));
+            PATH = String.join(File.separator, System.getenv("APPDATA"), "Dansoftware", "libraryapp2020", DIRECTORY_NAME);
         } else if (PlatformUtil.isLinux()) {
-            return new File(String.join(
-                    File.separator, System.getProperty("user.home"), ".libraryapp2020", "profiles", DIRECTORY_NAME
-            ));
+            PATH = String.join(File.separator, System.getProperty("user.home"), ".libraryapp2020", "profiles", DIRECTORY_NAME);
         } else if (PlatformUtil.isMac()) {
-            return new File(String.join(
-                    File.separator, "~", "Library", "Application", "Support", "Dansoftware", "libraryapp_2020", DIRECTORY_NAME
-            ));
+            PATH = String.join(File.separator, "~", "Library", "Application", "Support", "Dansoftware", "libraryapp2020", DIRECTORY_NAME);
         } else {
-            return new File(DIRECTORY_NAME);
+            PATH = DIRECTORY_NAME;
         }
     }
 
-    private PluginDirectory() {
+    public PluginDirectory() {
+        super(PATH);
     }
 
+    /**
+     * Returns all jar files located in the directory
+     *
+     * @return the array of {@link File} objects
+     */
+    public File[] getPluginFiles() {
+        Pattern pattern = Pattern.compile(".*\\.jar");
+        return this.listFiles((dir, name) -> pattern.matcher(name).matches());
+    }
+
+    /**
+     * Returns all jar files located in the directory as {@link URL} objects
+     *
+     * @return the array of {@link URL}s
+     */
+    public URL[] getPluginFileURLS() {
+        try {
+            return FileUtils.toURLs(getPluginFiles());
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
