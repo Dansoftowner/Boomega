5ad158db43f8f7700b3fd53e8ac1323b33a0c9dd
Daniel Gyorffy <judalion777@gmail.com>
2020-08-29 16:41:56 +0200

    Context docs

--- src/main/java/com/dansoftware/libraryapp/gui/entry/Context.java
+++ src/main/java/com/dansoftware/libraryapp/gui/entry/Context.java
@@ -5,26 +5,75 @@ import javafx.scene.layout.Region;
 
 import java.util.function.Consumer;
 
+/**
+ * A Context is an abstract structure that allows to interact with a particular GUI environment
+ * <i>(making alerts, showing notifications, etc...)</i>.
+ *
+ * @author Daniel Gyorffy
+ */
 public interface Context {
+
+    /**
+     * Shows a popup (on the center) with the GUI-element defined.
+     *
+     * @param region the {@link Region} GUI element to be shown
+     */
     void showOverlay(Region region);
 
+    /**
+     * Shows a popup (on the center) with the GUI-element defined.
+     *
+     * @param region the {@link Region} GUI element to be shown
+     * @param blocking {@code false} if clicking outside of the popup should close it.
+     */
     void showOverlay(Region region, boolean blocking);
 
+    /**
+     * Hides the popup (if it's showing).
+     *
+     * @param region to be hidden
+     */
     void hideOverlay(Region region);
 
+    /**
+     * Shows an error alert dialog.
+     *
+     * @param title the title of the dialog
+     * @param message the message of the dialog
+     * @param onResult the action when the user clicks the OK button
+     */
     void showErrorDialog(String title,
                          String message,
                          Consumer<ButtonType> onResult);
 
+    /**
+     * Shows an error alert dialog. Also, it can show
+     * the stacktrace of an exception.
+     *
+     * @param title the title of the dialog
+     * @param message the message of the dialog
+     * @param exception the exception that caused the dialog
+     * @param onResult the action when the user clicks the OK button
+     */
     void showErrorDialog(String title,
                          String message,
                          Exception exception,
                          Consumer<ButtonType> onResult);
 
+    /**
+     * Shows an information dialog.
+     *
+     * @param title the title
+     * @param message the message
+     * @param onResult the action that handles the button click-s on the dialog
+     */
     void showInformationDialog(String title,
                                String message,
                                Consumer<ButtonType> onResult);
 
+    /**
+     * Requests focus on the particular GUI environment (usually on a {@link javafx.stage.Window}).
+     */
     void requestFocus();
 
     default void showErrorDialog(String title, String message) {
