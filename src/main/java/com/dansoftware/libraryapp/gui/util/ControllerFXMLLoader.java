918d751f579bc507a30534aca192841802eb0968
Györffy Dániel <Gyrffy.Dniel@jedlik.helyi>
2020-09-04 11:30:46 +0200

    ControllerFXMLLoader

---
+++ src/main/java/com/dansoftware/libraryapp/gui/util/ControllerFXMLLoader.java
@@ -0,0 +1,33 @@
+package com.dansoftware.libraryapp.gui.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
+
+/**
+ * A {@link ControllerFXMLLoader} is an {@link FXMLLoader} implementation that can use
+ * a custom object as controller for the loaded fxml.
+ *
+ * @author Daniel Gyorffy
+ */
+public class ControllerFXMLLoader extends FXMLLoader {
+
+    public ControllerFXMLLoader(Initializable controller, URL location, ResourceBundle resources) {
+        super(location, resources, null, (Class<?> controllerClass) -> {
+            if (controller.getClass() != controllerClass) {
+                throw new ClassCastException(
+                        String.format(
+                                "Controller object '%s' cannot be used as a '%s'",
+                                controller.getClass(),
+                                controllerClass
+                        )
+                );
+            }
+
+            return controller;
+        }, StandardCharsets.UTF_8);
+    }
+}
