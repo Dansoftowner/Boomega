e5d6095c3baaa7c8cfe961cbf9b333acbd759ada
Györffy Dániel <Gyrffy.Dniel@jedlik.helyi>
2020-09-09 11:10:16 +0200

    FXCollectionUtils.copyOf utility method

--- src/main/java/com/dansoftware/libraryapp/gui/util/FXCollectionUtils.java
+++ src/main/java/com/dansoftware/libraryapp/gui/util/FXCollectionUtils.java
@@ -1,2 +1,17 @@
-package com.dansoftware.libraryapp.gui.util;public class FXCollectionUtils {
+package com.dansoftware.libraryapp.gui.util;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
+public class FXCollectionUtils {
+
+    private FXCollectionUtils() {
+    }
+
+    public static <T> ObservableList<T> copyOf(ObservableList<T> observableList) {
+        return FXCollections.observableArrayList(List.copyOf(observableList));
+    }
+
 }
