322465d5900736aab14dc9a3882d79d4ba313e1d
Daniel Gyorffy <judalion777@gmail.com>
2020-09-11 20:44:35 +0200

    DatabaseCreatorActivity docs

--- src/main/java/com/dansoftware/libraryapp/gui/dbcreator/DatabaseCreatorActivity.java
+++ src/main/java/com/dansoftware/libraryapp/gui/dbcreator/DatabaseCreatorActivity.java
@@ -8,7 +8,26 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
+/**
+ * A {@link DatabaseCreatorActivity} is used for showing a user-dialog for creating brand new databases.
+ *
+ * <p>
+ * It automatically updates the given {@link DatabaseTracker} when a new database is created.
+ *
+ * @author Daniel Gyorffy
+ */
 public class DatabaseCreatorActivity {
+
+    /**
+     * Shows the window and waits until it's closed.
+     *
+     * <p>
+     * It gives the created database result in an {@link Optional}.
+     *
+     * @param databaseTracker the {@link DatabaseTracker} that should be used for updating the saved databases
+     * @param owner the owner window for the form's window
+     * @return the wrapped result
+     */
     public Optional<DatabaseMeta> show(@NotNull DatabaseTracker databaseTracker, @Nullable Window owner) {
         DatabaseCreatorView view = new DatabaseCreatorView(databaseTracker);
         DatabaseCreatorWindow window = new DatabaseCreatorWindow(view, owner);
