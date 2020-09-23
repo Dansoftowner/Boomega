07aaba289156bcd1eb341d3c6f86d713449e3837
Daniel Gyorffy <judalion777@gmail.com>
2020-09-23 18:03:59 +0200

    Using UTF-8 charset for Preferences explicitly

--- src/main/java/com/dansoftware/libraryapp/appdata/Preferences.java
+++ src/main/java/com/dansoftware/libraryapp/appdata/Preferences.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -69,7 +70,7 @@ public class Preferences {
     private Preferences(@NotNull File file) throws IOException {
         this.sourceFile = file;
 
-        try (var reader = new BufferedReader(new FileReader(file))) {
+        try (var reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
             JsonObject temp = new Gson().fromJson(reader, JsonObject.class);
             this.jsonObject = Objects.isNull(temp) ? new JsonObject() : temp;
         } catch (JsonIOException | JsonSyntaxException | IOException e) {
