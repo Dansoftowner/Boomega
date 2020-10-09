6642b327fa34f49e7ef42d0be9f46db82fb86d85
Daniel Gyorffy <judalion777@gmail.com>
2020-10-09 19:48:51 +0200

    BackingStage utility

---
+++ src/main/java/com/dansoftware/libraryapp/gui/preloader/BackingStage.java
@@ -0,0 +1,35 @@
+package com.dansoftware.libraryapp.gui.preloader;
+
+import com.dansoftware.libraryapp.util.ReflectionUtils;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class BackingStage extends Stage {
+
+    public BackingStage() {
+        super(StageStyle.UTILITY);
+        setTitle(StringUtils.EMPTY);
+        setOpacity(0);
+    }
+
+    private <S extends Stage> S configureStage(S stage) {
+        stage.initOwner(this);
+        return stage;
+    }
+
+    public Stage createChild(@NotNull StageStyle stageStyle) {
+        return configureStage(new Stage(stageStyle));
+    }
+
+    public Stage createChild() {
+        return createChild(StageStyle.DECORATED);
+    }
+
+    public <S extends Stage> S createChild(@NotNull Class<S> classRef, Object... args)
+            throws ReflectiveOperationException {
+        S stage = ReflectionUtils.constructObject(classRef, args);
+        return configureStage(stage);
+    }
+}
