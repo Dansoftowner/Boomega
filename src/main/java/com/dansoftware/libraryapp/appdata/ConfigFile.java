cf5c5ff23ce1813bd1a91077f7e6501525b36881
Daniel Gyorffy <judalion777@gmail.com>
2020-10-09 14:31:22 +0200

    More flexible ConfigFile API

--- src/main/java/com/dansoftware/libraryapp/appdata/ConfigFile.java
+++ src/main/java/com/dansoftware/libraryapp/appdata/ConfigFile.java
@@ -1,25 +1,50 @@
 package com.dansoftware.libraryapp.appdata;
 
 import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 public class ConfigFile extends File {
 
-    private static final ConfigFile instance = new ConfigFile();
+    private static ConfigFile defaultFile;
 
     private final boolean nonExisted;
 
-    private ConfigFile() {
+    public ConfigFile() {
         super(new File(FileUtils.getUserDirectoryPath(), ".libraryapp2020"), "config.conf");
-        nonExisted = !exists();
+        nonExisted = !determineExists();
     }
 
     public boolean isNonExisted() {
         return nonExisted;
     }
 
+    protected boolean determineExists() {
+        return exists();
+    }
+
+    protected void create() throws IOException {
+        getParentFile().mkdirs();
+        createNewFile();
+    }
+
+    public InputStream openStream() throws IOException {
+        if (determineExists()) create();
+        return new FileInputStream(this);
+    }
+
+    public static void setDefault(@NotNull ConfigFile configFile) {
+        defaultFile = Objects.requireNonNull(configFile);
+    }
+
     public static ConfigFile getConfigFile() {
-        return instance;
+        if (defaultFile == null)
+            defaultFile = new ConfigFile();
+        return defaultFile;
     }
 }
