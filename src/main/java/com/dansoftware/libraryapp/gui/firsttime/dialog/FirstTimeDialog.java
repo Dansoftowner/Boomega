e195e9371c130ec5bc2205169fd3d3b359b5b9ba
Daniel Gyorffy <judalion777@gmail.com>
2020-10-14 18:17:31 +0200

    FirstTimeDialog devs

--- src/main/java/com/dansoftware/libraryapp/gui/firsttime/dialog/FirstTimeDialog.java
+++ src/main/java/com/dansoftware/libraryapp/gui/firsttime/dialog/FirstTimeDialog.java
@@ -4,11 +4,13 @@ import com.dansoftware.libraryapp.gui.theme.Theme;
 import com.dansoftware.libraryapp.gui.theme.Themeable;
 import com.dansoftware.libraryapp.locale.I18N;
 import com.dansoftware.sgmdialog.SegmentDialog;
+import jfxtras.styles.jmetro.JMetroStyleClass;
 
 public class FirstTimeDialog extends SegmentDialog implements Themeable {
 
     public FirstTimeDialog() {
         super(I18N.getFirstTimeDialogValues(), new DialogSegmentSequence());
+        getStyleClass().add(JMetroStyleClass.BACKGROUND);
         Theme.registerThemeable(this);
     }
 
