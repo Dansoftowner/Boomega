414b1ae3c022dfad7bf6200049e115da0a4e62f3
Györffy Dániel <Gyrffy.Dniel@jedlik.helyi>
2020-10-14 11:38:36 +0200

    Moving LoginDataSaving to a public class

---
+++ src/main/java/com/dansoftware/libraryapp/gui/login/form/LoginDataSaving.java
@@ -0,0 +1,42 @@
+package com.dansoftware.libraryapp.gui.login.form;
+
+import com.dansoftware.libraryapp.appdata.Preferences;
+import com.dansoftware.libraryapp.appdata.logindata.LoginData;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * A {@link LoginDataSaving} represents a {@link Thread} that is used for
+ * saving the {@link LoginData} into {@link Preferences}.
+ *
+ * @author Daniel Gyorffy
+ */
+public class LoginDataSaving extends Thread {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginDataSaving.class);
+
+    private final Preferences preferences;
+    private final LoginData loginData;
+
+    public LoginDataSaving(@NotNull Preferences preferences, @NotNull LoginData loginData) {
+        this.setName(getClass().getSimpleName());
+        this.preferences = preferences;
+        this.loginData = loginData;
+    }
+
+    @Override
+    public void run() {
+        try {
+            preferences.editor()
+                    .set(Preferences.Key.LOGIN_DATA, loginData)
+                    .commit();
+            logger.debug("Done");
+        } catch (IOException e) {
+            logger.error("Error saving LoginData", e);
+        }
+    }
+
+}
