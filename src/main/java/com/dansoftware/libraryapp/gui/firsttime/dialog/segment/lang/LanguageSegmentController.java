814d0ba90eef609c7d3d7a60acb6550b4758c949
Daniel Gyorffy <judalion777@gmail.com>
2020-10-24 21:16:12 +0200

    LanguageSegment 1.0

--- src/main/java/com/dansoftware/libraryapp/gui/firsttime/dialog/segment/lang/LanguageSegmentController.java
+++ src/main/java/com/dansoftware/libraryapp/gui/firsttime/dialog/segment/lang/LanguageSegmentController.java
@@ -1,13 +1,47 @@
 package com.dansoftware.libraryapp.gui.firsttime.dialog.segment.lang;
 
 import com.dansoftware.libraryapp.appdata.Preferences;
+import com.dansoftware.libraryapp.locale.I18N;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class LanguageSegmentController implements Initializable {
+/**
+ * Controller for the language-segment in the {@link com.dansoftware.libraryapp.gui.firsttime.dialog.FirstTimeDialog}.
+ *
+ * @author Daniel Gyorffy
+ */
+public class LanguageSegmentController
+        implements Initializable, ChangeListener<LanguageSegmentController.LanguageEntry> {
+
+    /**
+     * Represents an item in the ListView
+     */
+    static final class LanguageEntry {
+
+        private final Locale locale;
+
+        private LanguageEntry(Locale locale) {
+            this.locale = locale;
+        }
+
+        @Override
+        public String toString() {
+            return locale.getDisplayName();
+        }
+    }
+
+    @FXML
+    private ListView<LanguageEntry> listView;
 
     private final Preferences preferences;
 
@@ -15,9 +49,28 @@ public class LanguageSegmentController implements Initializable {
         this.preferences = preferences;
     }
 
+    @Override
+    public void changed(ObservableValue<? extends LanguageEntry> observable, LanguageEntry oldValue, LanguageEntry newValue) {
+        if (newValue != null) preferences.editor().put(Preferences.Key.LOCALE, newValue.locale);
+        else listView.getSelectionModel().select(oldValue); //we don't allow the user to choose no items
+    }
+
+    private void fillListView(ListView<LanguageEntry> listView) {
+        List<Locale> availableLocales = I18N.getAvailableLocales();
+        int defaultLocaleIndex = availableLocales.indexOf(Locale.getDefault());
+
+        listView.getItems().addAll(I18N.getAvailableLocales().stream()
+                .map(LanguageEntry::new)
+                .collect(Collectors.toList()));
+        listView.getSelectionModel().select(defaultLocaleIndex);
+        listView.scrollTo(defaultLocaleIndex);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        fillListView(listView);
+        listView.getSelectionModel()
+                .selectedItemProperty()
+                .addListener(this);
     }
 }
