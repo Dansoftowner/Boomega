eb2a6388f73ef9e91c2966d4af3eb11ca2e1a0fa
Daniel Gyorffy <judalion777@gmail.com>
2020-10-31 12:19:06 +0100

    Clean .kt s

--- src/main/java/com/dansoftware/libraryapp/gui/info/dependency/DependencyLister.kt
+++ src/main/java/com/dansoftware/libraryapp/gui/info/dependency/DependencyLister.kt
@@ -12,30 +12,27 @@ import org.jetbrains.annotations.NotNull
  */
 object DependencyLister {
     @JvmStatic
-    @NotNull
-    fun listDependencies(): List<DependencyInfo> {
-        return listOf(
-                DependencyInfo("JavaFX", "https://openjfx.io/", LicenseInfo.gnu2ClassPath()),
-                DependencyInfo("OpenJDK", "http://openjdk.java.net/", LicenseInfo.gnu2ClassPath()),
-                DependencyInfo("AnimateFX", "https://github.com/Typhon0/AnimateFX", LicenseInfo.apache20License()),
-                DependencyInfo("Apache Commons IO", "http://commons.apache.org/proper/commons-io/", LicenseInfo.apache20License()),
-                DependencyInfo("Apache Commons Lang", "https://commons.apache.org/proper/commons-lang/", LicenseInfo.apache20License()),
-                DependencyInfo("Gson", "https://github.com/google/gson", LicenseInfo.apache20License()),
-                DependencyInfo("Jetbrains annotations", "https://github.com/JetBrains/java-annotations", LicenseInfo.apache20License()),
-                DependencyInfo("Reflections", "https://github.com/ronmamo/reflections", LicenseInfo.wtfplLicense()),
-                DependencyInfo("SLF4J API", "http://www.slf4j.org/", LicenseInfo.mitLicense()),
-                DependencyInfo("Logback Project", "http://logback.qos.ch/", LicenseInfo("EPL v1.0 and LGPL 2.1", "http://logback.qos.ch/license.html")),
-                DependencyInfo("jUnit 5", "https://junit.org/junit5/", LicenseInfo("Eclipse Public License - v 2.0", "https://github.com/junit-team/junit5/blob/main/LICENSE.md")),
-                DependencyInfo("FXTaskbarProgressbar", "http://github.com/dansoftowner/fxtaskbarprogressbar", LicenseInfo.apache20License()),
-                DependencyInfo("jFileGoodies", "https://github.com/Dansoftowner/jFileGoodies", LicenseInfo.apache20License()),
-                DependencyInfo("JFoenix", "http://www.jfoenix.com/", LicenseInfo.apache20License()),
-                DependencyInfo("JMetro", "https://pixelduke.com/java-javafx-theme-jmetro/", LicenseInfo("New BSD license", "http://en.wikipedia.org/wiki/BSD_licenses#3-clause_license_.28.22Revised_BSD_License.22.2C_.22New_BSD_License.22.2C_or_.22Modified_BSD_License.22.29")),
-                DependencyInfo("ControlsFX", "https://github.com/controlsfx/controlsfx", LicenseInfo("BSD 3-Clause License", "https://github.com/controlsfx/controlsfx/blob/master/license.txt")),
-                DependencyInfo("WorkbenchFX (+ with patches)", "https://github.com/dlsc-software-consulting-gmbh/WorkbenchFX", LicenseInfo.apache20License()),
-                DependencyInfo("MDFX", "https://github.com/JPro-one/markdown-javafx-renderer", LicenseInfo.apache20License()),
-                DependencyInfo("Nitrite Database", "https://github.com/nitrite/nitrite-java", LicenseInfo.apache20License()),
-                DependencyInfo("JUnique", "http://www.sauronsoftware.it/projects/junique/", LicenseInfo("LGPL", null)),
-                DependencyInfo("PDFViewerFX", "https://github.com/Dansoftowner/PDFViewerFX", LicenseInfo.apache20License())
-        )
-    }
+    fun listDependencies(): @NotNull List<DependencyInfo> = listOf(
+            DependencyInfo("JavaFX", "https://openjfx.io/", LicenseInfo.gnu2ClassPath()),
+            DependencyInfo("OpenJDK", "http://openjdk.java.net/", LicenseInfo.gnu2ClassPath()),
+            DependencyInfo("AnimateFX", "https://github.com/Typhon0/AnimateFX", LicenseInfo.apache20License()),
+            DependencyInfo("Apache Commons IO", "http://commons.apache.org/proper/commons-io/", LicenseInfo.apache20License()),
+            DependencyInfo("Apache Commons Lang", "https://commons.apache.org/proper/commons-lang/", LicenseInfo.apache20License()),
+            DependencyInfo("Gson", "https://github.com/google/gson", LicenseInfo.apache20License()),
+            DependencyInfo("Jetbrains annotations", "https://github.com/JetBrains/java-annotations", LicenseInfo.apache20License()),
+            DependencyInfo("Reflections", "https://github.com/ronmamo/reflections", LicenseInfo.wtfplLicense()),
+            DependencyInfo("SLF4J API", "http://www.slf4j.org/", LicenseInfo.mitLicense()),
+            DependencyInfo("Logback Project", "http://logback.qos.ch/", LicenseInfo("EPL v1.0 and LGPL 2.1", "http://logback.qos.ch/license.html")),
+            DependencyInfo("jUnit 5", "https://junit.org/junit5/", LicenseInfo("Eclipse Public License - v 2.0", "https://github.com/junit-team/junit5/blob/main/LICENSE.md")),
+            DependencyInfo("FXTaskbarProgressbar", "http://github.com/dansoftowner/fxtaskbarprogressbar", LicenseInfo.apache20License()),
+            DependencyInfo("jFileGoodies", "https://github.com/Dansoftowner/jFileGoodies", LicenseInfo.apache20License()),
+            DependencyInfo("JFoenix", "http://www.jfoenix.com/", LicenseInfo.apache20License()),
+            DependencyInfo("JMetro", "https://pixelduke.com/java-javafx-theme-jmetro/", LicenseInfo("New BSD license", "http://en.wikipedia.org/wiki/BSD_licenses#3-clause_license_.28.22Revised_BSD_License.22.2C_.22New_BSD_License.22.2C_or_.22Modified_BSD_License.22.29")),
+            DependencyInfo("ControlsFX", "https://github.com/controlsfx/controlsfx", LicenseInfo("BSD 3-Clause License", "https://github.com/controlsfx/controlsfx/blob/master/license.txt")),
+            DependencyInfo("WorkbenchFX (+ with patches)", "https://github.com/dlsc-software-consulting-gmbh/WorkbenchFX", LicenseInfo.apache20License()),
+            DependencyInfo("MDFX", "https://github.com/JPro-one/markdown-javafx-renderer", LicenseInfo.apache20License()),
+            DependencyInfo("Nitrite Database", "https://github.com/nitrite/nitrite-java", LicenseInfo.apache20License()),
+            DependencyInfo("JUnique", "http://www.sauronsoftware.it/projects/junique/", LicenseInfo("LGPL", null)),
+            DependencyInfo("PDFViewerFX", "https://github.com/Dansoftowner/PDFViewerFX", LicenseInfo.apache20License())
+    )
 }
--- src/main/java/com/dansoftware/libraryapp/gui/util/I18NButtonTypes.kt
+++ src/main/java/com/dansoftware/libraryapp/gui/util/I18NButtonTypes.kt
@@ -12,24 +12,30 @@ import javafx.scene.control.ButtonType
 object I18NButtonTypes {
     @JvmField
     val APPLY = createButtonType("Dialog.apply.button", ButtonBar.ButtonData.APPLY)
+
     @JvmField
     val OK = createButtonType("Dialog.ok.button", ButtonBar.ButtonData.OK_DONE)
+
     @JvmField
     val CANCEL = createButtonType("Dialog.cancel.button", ButtonBar.ButtonData.CANCEL_CLOSE)
+
     @JvmField
     val CLOSE = createButtonType("Dialog.close.button", ButtonBar.ButtonData.CANCEL_CLOSE)
+
     @JvmField
     val YES = createButtonType("Dialog.yes.button", ButtonBar.ButtonData.YES)
+
     @JvmField
     val NO = createButtonType("Dialog.no.button", ButtonBar.ButtonData.NO)
+
     @JvmField
     val FINISH = createButtonType("Dialog.finish.button", ButtonBar.ButtonData.FINISH)
+
     @JvmField
     val NEXT = createButtonType("Dialog.next.button", ButtonBar.ButtonData.NEXT_FORWARD)
+
     @JvmField
     val PREVIOUS = createButtonType("Dialog.previous.button", ButtonBar.ButtonData.BACK_PREVIOUS)
 
-    private fun createButtonType(key: String, buttonData: ButtonBar.ButtonData): ButtonType {
-        return ButtonType(I18N.getButtonTypeValues().getString(key), buttonData)
-    }
+    private fun createButtonType(key: String, buttonData: ButtonBar.ButtonData) = ButtonType(I18N.getButtonTypeValues().getString(key), buttonData)
 }
