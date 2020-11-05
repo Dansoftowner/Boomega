3e7c494257ac1e32db73786f31ae2fddf30e57c9
Daniel Gyorffy <judalion777@gmail.com>
2020-11-05 16:24:26 +0100

    SystemBrowser.java to .kt

---
+++ src/main/java/com/dansoftware/libraryapp/util/SystemBrowser.kt
@@ -0,0 +1,27 @@
+package com.dansoftware.libraryapp.util
+
+import java.awt.Desktop
+import java.io.IOException
+import java.net.URISyntaxException
+import java.net.URL
+
+/**
+ * SystemBrowser is a utility for communicating with the default browser of the system.
+ *
+ * @author Daniel Gyorffy
+ */
+class SystemBrowser {
+    companion object {
+        @JvmStatic
+        fun isSupported(): Boolean = Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)
+    }
+
+    fun browse(url: String) =
+            try {
+                Desktop.getDesktop().browse(URL(url).toURI())
+            } catch (e: IOException) {
+                throw RuntimeException(e)
+            } catch (e: URISyntaxException) {
+                throw RuntimeException(e)
+            }
+}
