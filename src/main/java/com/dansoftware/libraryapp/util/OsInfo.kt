9486661d665c6d0c85c9134f3d9c86ab4dac033c
Daniel Gyorffy <judalion777@gmail.com>
2020-11-06 11:22:07 +0100

    Optimizing OsInfo

--- src/main/java/com/dansoftware/libraryapp/util/OsInfo.kt
+++ src/main/java/com/dansoftware/libraryapp/util/OsInfo.kt
@@ -10,17 +10,31 @@ import oshi.SystemInfo
  */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 object OsInfo {
+    private val platformType: PlatformEnum = SystemInfo.getCurrentPlatformEnum()
+    private val version: String = SystemInfo().operatingSystem.versionInfo.version
+
+    @JvmStatic
+    fun getPlatformType(): PlatformEnum = platformType
+
+    @JvmStatic
+    fun getPlatformVersion(): String = version
+
     @JvmStatic
     fun hasTypeAndVersion(platformType: PlatformEnum, versionStarts: String): Boolean =
-            hasType(platformType) && SystemInfo().operatingSystem.versionInfo.version.startsWith(versionStarts)
+            hasType(platformType) && this.version.startsWith(versionStarts)
+
     @JvmStatic
-    fun hasType(platformType: PlatformEnum): Boolean = (SystemInfo.getCurrentPlatformEnum() == platformType)
+    fun hasType(platformType: PlatformEnum): Boolean = platformType == platformType
+
     @JvmStatic
     fun isWindows(): Boolean = hasType(PlatformEnum.WINDOWS)
+
     @JvmStatic
     fun isLinux(): Boolean = hasType(PlatformEnum.LINUX)
+
     @JvmStatic
     fun isMac(): Boolean = hasType(PlatformEnum.MACOSX)
+
     @JvmStatic
     fun isWindows10(): Boolean = hasTypeAndVersion(PlatformEnum.WINDOWS, "10")
 }
