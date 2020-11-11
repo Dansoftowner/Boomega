5ddf5c12dbca7254b5fbf0a0ce5605cedd173d23
Daniel Gyorffy <judalion777@gmail.com>
2020-11-11 10:48:32 +0100

    UpdateSearcher abstraction

--- src/main/java/com/dansoftware/libraryapp/update/UpdateSearcher.java
+++ src/main/java/com/dansoftware/libraryapp/update/UpdateSearcher.java
@@ -1,41 +1,39 @@
 package com.dansoftware.libraryapp.update;
 
 import com.dansoftware.libraryapp.util.adapter.VersionInteger;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
- * An UpdateSearcher used for searching for updates.
+ * Abstraction for an object that can search for updates in a particular way.
+ *
+ * <p>
+ * Also, this class should be used when we want to create an {@link UpdateSearcher} object.
+ * We use the static {@link #newInstance(VersionInteger)} method for creating the particular instance,
+ * because it provides testability.
+ *
+ * Example:
+ * <pre>{@code
+ * UpdateSearcher updateSearcher = UpdateSearcher.newInstance(...);
+ * //...using it
+ * }</pre>
  *
  * @author Daniel Gyorffy
  */
-public class UpdateSearcher {
+@SuppressWarnings("StaticInitializerReferencesSubClass")
+public abstract class UpdateSearcher {
 
-    /**
-     * The updater server-address
-     */
-    private static final String LOCATION = "https://update-server-ed6c3.firebaseio.com/libraryapp.json";
+    private static final Function<@NotNull VersionInteger, @NotNull UpdateSearcher> DEFAULT_INSTANCE_FACTORY;
+    private static Function<@NotNull VersionInteger, @NotNull UpdateSearcher> instanceFactory;
 
-    private final VersionInteger base;
-
-    /**
-     * Creates a basic update searcher object;
-     *
-     * @param base the base version that the object should compare to; mustn't be null
-     * @throws NullPointerException if base is null.
-     */
-    public UpdateSearcher(@NotNull VersionInteger base) {
-        this.base = Objects.requireNonNull(base, "The base mustn't be null");
+    static {
+        DEFAULT_INSTANCE_FACTORY = NetworkUpdateSearcher::new;
+        instanceFactory = DEFAULT_INSTANCE_FACTORY;
     }
 
     /**
@@ -45,24 +43,7 @@ public class UpdateSearcher {
      * @return the result of the search
      */
     @NotNull
-    public UpdateSearchResult search() {
-        UpdateSearchResult result = new UpdateSearchResult();
-        try {
-            var information = loadInfo();
-            if (information != null) {
-                VersionInteger newVersion = new VersionInteger(information.getVersion());
-                if (base.isOlderThan(newVersion)) {
-                    result.newUpdate = true;
-                    result.information = information;
-                }
-            }
-        } catch (IOException | RuntimeException e) {
-            result.failed = true;
-            result.failedCause = e;
-        }
-
-        return result;
-    }
+    public abstract UpdateSearchResult search();
 
     /**
      * Loads the update-information from the server into a {@link UpdateInformation} object.
@@ -70,25 +51,29 @@ public class UpdateSearcher {
      * @return the {@link UpdateInformation} object.
      * @throws IOException if some IO exception occurs
      */
-    private UpdateInformation loadInfo() throws IOException {
-        URL url = new URL(LOCATION);
-        URLConnection connection = url.openConnection();
-        connection.connect();
+    @Nullable
+    protected abstract UpdateInformation loadInfo() throws IOException;
 
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(UpdateInformation.class, new UpdateInformationDeserializer())
-                .create();
+    /**
+     * Creates a particular {@link UpdateSearcher} object.
+     *
+     * @param base the base version that the object should compare to; mustn't be null
+     * @return the object instance
+     */
+    public static UpdateSearcher newInstance(@NotNull VersionInteger base) {
+        return instanceFactory.apply(base);
+    }
 
-        try (var reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            return gson.fromJson(reader, UpdateInformation.class);
-        }
+    static void setInstanceFactory(@NotNull Function<@NotNull VersionInteger, @NotNull UpdateSearcher> instanceFactory) {
+        Objects.requireNonNull(instanceFactory);
+        UpdateSearcher.instanceFactory = instanceFactory;
     }
 
     /**
      * An UpdateSearchResult object can hold all information
      * about an update-search.
      *
-     * @see UpdateSearcher#search()
+     * @see NetworkUpdateSearcher#search()
      */
     public static final class UpdateSearchResult {
         private boolean newUpdate;
@@ -134,6 +119,22 @@ public class UpdateSearcher {
             return information;
         }
 
+        void setNewUpdate(boolean newUpdate) {
+            this.newUpdate = newUpdate;
+        }
+
+        void setFailed(boolean failed) {
+            this.failed = failed;
+        }
+
+        void setFailedCause(Exception failedCause) {
+            this.failedCause = failedCause;
+        }
+
+        void setInformation(UpdateInformation information) {
+            this.information = information;
+        }
+
         /**
          * This:
          * <pre>{@code
@@ -155,7 +156,7 @@ public class UpdateSearcher {
          * @see #getFailedCause()
          */
         @NotNull
-        public UpdateSearchResult ifFailed(@NotNull Consumer<Exception> onFailed) {
+        public NetworkUpdateSearcher.UpdateSearchResult ifFailed(@NotNull Consumer<Exception> onFailed) {
             if (failed)
                 onFailed.accept(failedCause);
             return this;
@@ -182,7 +183,7 @@ public class UpdateSearcher {
          * @see #getInformation()
          */
         @NotNull
-        public UpdateSearchResult ifNewUpdateAvailable(@NotNull Consumer<UpdateInformation> onAvailable) {
+        public NetworkUpdateSearcher.UpdateSearchResult ifNewUpdateAvailable(@NotNull Consumer<UpdateInformation> onAvailable) {
             if (newUpdate)
                 onAvailable.accept(information);
             return this;
@@ -210,7 +211,7 @@ public class UpdateSearcher {
          * @see #getInformation()
          */
         @NotNull
-        public UpdateSearchResult ifNoUpdateAvailable(@NotNull Consumer<UpdateInformation> handler) {
+        public NetworkUpdateSearcher.UpdateSearchResult ifNoUpdateAvailable(@NotNull Consumer<UpdateInformation> handler) {
             if (!failed && !newUpdate)
                 handler.accept(information);
             return this;
