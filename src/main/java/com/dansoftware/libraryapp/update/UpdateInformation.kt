3dc1d11d7b4d2e2331b615b14e40b8691920f6ff
Daniel Gyorffy <judalion777@gmail.com>
2020-11-11 10:00:44 +0100

    Fixing some UpdateInformation docs

--- src/main/java/com/dansoftware/libraryapp/update/UpdateInformation.kt
+++ src/main/java/com/dansoftware/libraryapp/update/UpdateInformation.kt
@@ -3,14 +3,14 @@ package com.dansoftware.libraryapp.update
 /**
  * This class responsible for storing the information about
  * an update
+ *
+ * @author Daniel Gyorffy
  */
 class UpdateInformation
 /**
  * This constructor creates an UpdateInformationObject with the required values
  *
  * @param version   the new version of the update
- * @param reviewUrl the location of the review web page that describes
- * the features of the new update (http://example.com/libraryappreview.html).
- * @param binaries  the Map that contains the binary types and the location
- * of each downloadable binary on the web.
+ * @param reviewUrl the location of the review markdown-text.
+ * @param binaries  the List that contains the object representations of the downloadable binaries
  */(val version: String, val reviewUrl: String, val binaries: List<DownloadableBinary>)
