fb6eeed906b1b1e7aa72869d74aa82adfefc6a75
Daniel Gyorffy <judalion777@gmail.com>
2020-11-15 11:54:40 +0100

    Improving LanguagePack - registering locales + isRTL()

--- src/main/java/com/dansoftware/libraryapp/locale/LanguagePack.java
+++ src/main/java/com/dansoftware/libraryapp/locale/LanguagePack.java
@@ -3,10 +3,10 @@ package com.dansoftware.libraryapp.locale;
 import com.dansoftware.libraryapp.plugin.PluginClassLoader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Locale;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * A {@link LanguagePack} provides {@link ResourceBundle}s for a particular {@link Locale}.
@@ -14,6 +14,32 @@ import java.util.ResourceBundle;
  * @author Daniel Gyorffy
  */
 public abstract class LanguagePack {
+
+    private static final Logger logger = LoggerFactory.getLogger(LanguagePack.class);
+
+    private static final Map<Locale, List<Class<? extends LanguagePack>>> languagePacksForLocales;
+
+    static {
+        languagePacksForLocales = Collections.synchronizedMap(new HashMap<>());
+    }
+
+    protected static void registerLanguagePack(@NotNull Locale locale, @NotNull Class<? extends LanguagePack> languagePackClass) {
+        List<Class<? extends LanguagePack>> classes = languagePacksForLocales.get(locale);
+        if (classes != null) {
+            classes.add(languagePackClass);
+        } else {
+            languagePacksForLocales.put(locale, new LinkedList<>(List.of(languagePackClass)));
+        }
+    }
+
+    @NotNull
+    protected static List<Class<? extends LanguagePack>> getLanguagePacksForLocale(@NotNull Locale locale) {
+        return Optional.ofNullable(languagePacksForLocales.get(locale)).orElseGet(Collections::emptyList);
+    }
+
+    protected static Set<Locale> getSupportedLocales() {
+        return languagePacksForLocales.keySet();
+    }
 
     private final Locale locale;
     private LanguageTranslator languageTranslator;
@@ -39,6 +65,14 @@ public abstract class LanguagePack {
     protected ResourceBundle getBundle(String path) {
         return ResourceBundle.getBundle(path, locale, PluginClassLoader.getInstance());
     }
+
+    /**
+     * Returns {@code true} if the language represented by this pack is a <i>RIGHT TO LEFT</i>
+     * language like arabic or hebrew or not.
+     *
+     * @return {@code true} if the language is RTL; {@code false} otherwise
+     */
+    protected abstract boolean isRTL();
 
     @NotNull
     public abstract ResourceBundle getButtonTypeValues();
