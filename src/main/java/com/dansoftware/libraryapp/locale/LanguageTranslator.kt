03560d3a491c421357c7c0623ed0098a917136cb
Daniel Gyorffy <judalion777@gmail.com>
2020-11-23 15:07:24 +0100

    LanguageTranslator.getDisplayName utility

--- src/main/java/com/dansoftware/libraryapp/locale/LanguageTranslator.kt
+++ src/main/java/com/dansoftware/libraryapp/locale/LanguageTranslator.kt
@@ -1,6 +1,15 @@
 package com.dansoftware.libraryapp.locale
 
+import java.util.*
+
 /**
  * A [LanguageTranslator] represents a person who translated the app into a particular language
  */
-data class LanguageTranslator(val firstName: String, val lastName: String, val email: String)
+data class LanguageTranslator(val firstName: String, val lastName: String, val email: String) {
+    fun getDisplayName(locale: Locale): String {
+        return when (locale.language) {
+            "hu" -> lastName.plus(" ").plus(firstName)
+            else -> firstName.plus(" ").plus(lastName)
+        }
+    }
+}
