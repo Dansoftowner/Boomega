cafe5a56768bc49c048dacf625b2cd12b9da893b
Daniel Gyorffy <judalion777@gmail.com>
2020-11-28 14:27:47 +0100

    Package-info in ...window.undecorate

---
+++ src/main/java/com/dansoftware/libraryapp/gui/window/undecorate/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains tools for undecorating the particular window
+ * natively on a Window system.
+ *
+ * It isn't used yet because unfortunately the JavaFX environment
+ * reacts to it negatively because of some bugs, but it may work
+ * in the future.
+ *
+ * @author Daniel Gyorffy
+ */
+@Deprecated
+package com.dansoftware.libraryapp.gui.window.undecorate;
