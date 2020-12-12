e66d9cad7a223dbdfde6946aa06e5294959a0dae
Daniel Gyorffy <judalion777@gmail.com>
2020-12-12 20:30:50 +0100

    New features in PluginClassLoader

--- src/main/java/com/dansoftware/libraryapp/plugin/PluginClassLoader.kt
+++ src/main/java/com/dansoftware/libraryapp/plugin/PluginClassLoader.kt
@@ -1,6 +1,9 @@
 package com.dansoftware.libraryapp.plugin
 
+import com.dansoftware.libraryapp.util.ReflectionUtils
+import org.slf4j.LoggerFactory
 import java.net.URLClassLoader
+import java.util.*
 
 /**
  * The [PluginClassLoader] can load classes from both the class-path and the third-party plugins located
@@ -9,6 +12,9 @@ import java.net.URLClassLoader
  * @author Daniel Gyorffy
  */
 object PluginClassLoader : URLClassLoader(PluginDirectory.getPluginFilesAsUrls(), getSystemClassLoader()) {
+
+    @JvmStatic
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     /**
      * For already existing code bases
@@ -23,5 +29,29 @@ object PluginClassLoader : URLClassLoader(PluginDirectory.getPluginFilesAsUrls()
     override fun close() {
         super.close()
         PluginDirectory.clear()
+    }
+
+    /**
+     * Collects all subtypes of the given class
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun <T, S : T> getSubtypesOf(classRef: Class<T>): Set<Class<S>> {
+        return when {
+            this.isEmpty().not() -> ReflectionUtils.getSubtypesOf(classRef, this) as Set<Class<S>>
+            else -> Collections.emptySet()
+        }
+    }
+
+    /**
+     * Collects and initializes the subclasses of the given class
+     */
+    fun <T> initializeSubtypeClasses(classRef: Class<T>) {
+        getSubtypesOf(classRef).forEach {
+            try {
+                ReflectionUtils.initializeClass(it, getInstance())
+            } catch (e: ExceptionInInitializerError) {
+                logger.error("Failed to initialize a plugin class called '{}'", classRef.name, e)
+            }
+        }
     }
 }
