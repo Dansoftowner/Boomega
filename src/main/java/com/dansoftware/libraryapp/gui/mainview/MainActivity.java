d5556ad4b1cf628a46519e333128c8000e7e55b3
Daniel Gyorffy <judalion777@gmail.com>
2020-12-15 14:42:23 +0100

    🩹 MainActivity tracking made usable

--- src/main/java/com/dansoftware/libraryapp/gui/mainview/MainActivity.java
+++ src/main/java/com/dansoftware/libraryapp/gui/mainview/MainActivity.java
@@ -11,28 +11,29 @@ import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.lang.ref.WeakReference;
+import java.util.*;
 
 public class MainActivity implements ContextTransformable {
+
+    private static final Set<WeakReference<MainActivity>> instances = Collections.synchronizedSet(new HashSet<>());
 
     private final BooleanProperty showing;
     private final Preferences preferences;
     private final DatabaseTracker databaseTracker;
+    private final MainView mainView;
     private Database database;
-
-    private MainView mainView;
-
 
     public MainActivity(@NotNull Database database, @NotNull Preferences preferences, @NotNull DatabaseTracker databaseTracker) {
         this.database = Objects.requireNonNull(database, "The database mustn't be null");
         this.preferences = Objects.requireNonNull(preferences);
         this.databaseTracker = Objects.requireNonNull(databaseTracker);
         this.showing = new SimpleBooleanProperty();
+        this.mainView = new MainView(this, database, preferences, databaseTracker);
+        instances.add(new WeakReference<>(this));
     }
 
     public boolean show() {
-        this.mainView = new MainView(database, preferences, databaseTracker);
         new MainWindow(mainView).show();
         return true;
     }
@@ -51,6 +52,10 @@ public class MainActivity implements ContextTransformable {
     }
 
     public static Optional<MainActivity> getByDatabase(DatabaseMeta databaseMeta) {
-        return Optional.empty();
+        return instances.stream()
+                .map(WeakReference::get)
+                .filter(Objects::nonNull)
+                .filter(activity -> activity.database.getMeta().equals(databaseMeta))
+                .findAny();
     }
 }
