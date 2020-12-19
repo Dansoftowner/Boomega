29553efafe7b323bedeec564721066b4373a921f
Daniel Gyorffy <judalion777@gmail.com>
2020-12-19 19:04:57 +0100

    :sparkles: Improving MainWindow

--- src/main/java/com/dansoftware/libraryapp/gui/mainview/MainWindow.kt
+++ src/main/java/com/dansoftware/libraryapp/gui/mainview/MainWindow.kt
@@ -2,6 +2,12 @@ package com.dansoftware.libraryapp.gui.mainview
 
 import com.dansoftware.libraryapp.gui.window.BaseWindow
 
-private class MainWindow(view: MainView) : BaseWindow<MainView>("window.main.title", view) {
-
+private class MainWindow(view: MainView) :
+    BaseWindow<MainView>("window.main.title", "-", view.openedDatabase.toString(), view) {
+    init {
+        this.isMaximized = true
+        this.exitDialog = true
+        this.minWidth = 530.0
+        this.minHeight = 530.0
+    }
 }
