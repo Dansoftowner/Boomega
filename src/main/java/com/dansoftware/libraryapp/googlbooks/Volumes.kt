073fb38611b67628189178ca29eb50a5eb601165
Daniel Gyorffy <judalion777@gmail.com>
2020-12-22 15:33:49 +0100

    :sparkles: Classes for representing Google Books data

---
+++ src/main/java/com/dansoftware/libraryapp/googlbooks/Volumes.kt
@@ -0,0 +1,63 @@
+package com.dansoftware.libraryapp.googlbooks
+
+/**
+ * Represents a collection of [Volume]s pulled from a Google Books server
+ *
+ * @author Daniel Gyorffy
+ */
+class Volumes : Iterable<Volume> {
+    var kind: String? = null
+    var totalItems: Int = 0
+    var items: List<Volume>? = null
+
+    override fun iterator() = items?.iterator() ?: emptyList<Volume>().iterator()
+}
+
+/**
+ * Represents a [Volume] that holds the information of a Google Book
+ *
+ * @author Daniel Gyorffy
+ */
+data class Volume(var id: String?) {
+    var kind: String? = null
+    var selfLink: String? = null
+    var volumeInfo: VolumeInfo? = null
+
+    class VolumeInfo {
+        var title: String? = null
+        var subtitle: String? = null
+        var authors: List<String>? = null
+        var publisher: String? = null
+        var publishedDate: String? = null
+        var description: String? = null
+        var industryIdentifiers: List<IndustryIdentifier>? = null
+        var categories: List<String>? = null
+        var imageLinks: ImageLinks? = null
+        var language: String? = null
+        var previewLink: String? = null
+
+        class ImageLinks {
+            var extraLarge: String? = null
+            var large: String? = null
+            var medium: String? = null
+            var small: String? = null
+            var smallThumbnail: String? = null
+            var thumbnail: String? = null
+        }
+
+        class IndustryIdentifier {
+            var type: String? = null
+            var identifier: String? = null
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
