11cd81308f63e368173b08e7114d68e48b4fc61c
Daniel Gyorffy <judalion777@gmail.com>
2020-12-30 17:08:22 +0100

    :sparkles: SelectableLabel utility

--- src/main/java/com/dansoftware/libraryapp/gui/util/BaseFXUtils.kt
+++ src/main/java/com/dansoftware/libraryapp/gui/util/BaseFXUtils.kt
@@ -122,6 +122,16 @@ fun KClass<*>.loadImageResource(resource: String): Image {
     }
 }
 
+class SelectableLabel(text: String? = null) : TextField(text) {
+    init {
+        this.styleClass.remove("text-field")
+        this.style = "-fx-background-color: transparent;-fx-padding: 0;"
+        this.prefColumnCount = 15
+        this.styleClass.add("label")
+        this.isEditable = false
+    }
+}
+
 /**
  * The [RadioToggleButton] is a [RadioButton] styled as a [ToggleButton]
  */
