d7a132cc546d3fd14bdc435d7dd430d8b5df2637
Daniel Gyorffy <judalion777@gmail.com>
2021-01-03 15:32:28 +0100

    :art: Magazine structure updated

--- src/main/java/com/dansoftware/libraryapp/db/data/Magazine.kt
+++ src/main/java/com/dansoftware/libraryapp/db/data/Magazine.kt
@@ -2,7 +2,6 @@ package com.dansoftware.libraryapp.db.data
 
 import org.dizitart.no2.NitriteId
 import org.dizitart.no2.objects.Id
-import java.util.*
 
 /**
  * Represents a Magazine/Newspaper record in the database.
@@ -11,10 +10,10 @@ import java.util.*
  */
 data class Magazine(
     @field:Id val id: NitriteId,
-    var publisher: String,
-    var magazineName: String,
-    var title: String,
-    var publishedDate: Date
+    var publisher: String?,
+    var magazineName: String?,
+    var title: String?,
+    var publishedDate: String?
 ) {
     var serviceConnection: ServiceConnection? = null
 }
