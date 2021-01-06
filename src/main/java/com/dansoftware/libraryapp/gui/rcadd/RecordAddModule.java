7c22e812db48c5e4c1dbfcb9140052f11ac48a3e
Daniel Gyorffy <judalion777@gmail.com>
2021-01-06 18:11:58 +0100

    :poop: :clown_face: Adding record type chooser item to RecordAddModule

--- src/main/java/com/dansoftware/libraryapp/gui/rcadd/RecordAddModule.java
+++ src/main/java/com/dansoftware/libraryapp/gui/rcadd/RecordAddModule.java
@@ -2,16 +2,64 @@ package com.dansoftware.libraryapp.gui.rcadd;
 
 import com.dansoftware.libraryapp.locale.I18N;
 import com.dlsc.workbenchfx.model.WorkbenchModule;
+import com.dlsc.workbenchfx.view.controls.ToolbarItem;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
 
 public class RecordAddModule extends WorkbenchModule {
+
+    private final ObjectProperty<RecordAddForm> content =
+            new SimpleObjectProperty<>();
+
     public RecordAddModule() {
         super(I18N.getRecordAddFormValue("record.add.module.title"), MaterialDesignIcon.PLUS_BOX);
+        buildToolbar();
+    }
+
+    private void buildToolbar() {
+        this.getToolbarControlsLeft().add(buildRecordTypeChooserItem());
+    }
+
+    private ToolbarItem buildRecordTypeChooserItem() {
+        var toolbarItem = new ToolbarItem();
+        var toggleGroup = new ToggleGroup();
+        toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) ->
+                toolbarItem.setText(((MenuItem) newValue).getText())
+        );
+        toolbarItem.getItems().add(new RadioMenuItem(I18N.getRecordAddFormValue("record.add.rectype.book")) {{
+            content.addListener((observable, oldValue, newValue) -> {
+                if (newValue != null)
+                    setSelected(RecordAddForm.RecordType.BOOK.equals(newValue.recordTypeProperty().get()));
+            });
+            this.setToggleGroup(toggleGroup);
+            this.setOnAction(event -> content.get().recordTypeProperty().set(RecordAddForm.RecordType.BOOK));
+        }});
+        toolbarItem.getItems().add(new RadioMenuItem(I18N.getRecordAddFormValue("record.add.rectype.magazine")) {{
+            content.addListener((observable, oldValue, newValue) -> {
+                if (newValue != null)
+                    setSelected(RecordAddForm.RecordType.MAGAZINE.equals(newValue.recordTypeProperty().get()));
+            });
+            this.setToggleGroup(toggleGroup);
+            this.setOnAction(event -> content.get().recordTypeProperty().set(RecordAddForm.RecordType.MAGAZINE));
+        }});
+        return toolbarItem;
     }
 
     @Override
     public Node activate() {
-        return new RecordAddForm(RecordAddForm.RecordType.BOOK);
+        if (content.get() == null)
+            content.set(new RecordAddForm(RecordAddForm.RecordType.BOOK));
+        return content.get();
+    }
+
+    @Override
+    public boolean destroy() {
+        this.content.set(null);
+        return true;
     }
 }
