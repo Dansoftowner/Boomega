033105695314d69203f25520a14611ba0dfa5e0d
Daniel Gyorffy <judalion777@gmail.com>
2021-01-16 19:14:31 +0100

    :construction: :sparkles: Improving GoogleBookTile

--- src/main/java/com/dansoftware/libraryapp/gui/googlebooks/tile/GoogleBookTile.kt
+++ src/main/java/com/dansoftware/libraryapp/gui/googlebooks/tile/GoogleBookTile.kt
@@ -3,18 +3,24 @@ package com.dansoftware.libraryapp.gui.googlebooks.tile
 import com.dansoftware.libraryapp.googlebooks.Volume
 import com.dansoftware.libraryapp.gui.context.Context
 import com.dansoftware.libraryapp.gui.googlebooks.GoogleBookDetailsOverlay
+import com.dansoftware.libraryapp.gui.util.SelectableLabel
+import com.dansoftware.libraryapp.gui.window.undecorate.control.TitleBar
 import com.dansoftware.libraryapp.locale.I18N
-import com.fasterxml.jackson.databind.type.PlaceholderForType
+import javafx.geometry.Insets
+import javafx.geometry.Pos
 import javafx.scene.Group
 import javafx.scene.control.Label
+import javafx.scene.control.Separator
+import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.input.MouseButton
 import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
 import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
 
 class GoogleBookTile(private val context: Context, private val volume: Volume) :
-    HBox(5.0, Thumbnail(volume), Info(volume)) {
+    HBox(10.0, Thumbnail(volume), Info(volume)) {
 
     init {
         styleClass.add("google-book-tile")
@@ -42,15 +48,42 @@ class GoogleBookTile(private val context: Context, private val volume: Volume) :
         private class PlaceHolder : Label(I18N.getGoogleBooksImportValue("google.books.table.thumbnail.not.available"))
     }
 
-    private class Info(volume: Volume) : StackPane() {
+    private class Info(volume: Volume) : VBox(5.0) {
 
         init {
-            children.add(Group(buildVBox(volume)))
+            setHgrow(this, Priority.ALWAYS)
+            children.add(buildMarkLabel())
+            children.add(Separator())
+            children.add(buildVBox(volume))
         }
+
+        private fun buildMarkLabel() =
+            HBox(10.0, ImageView(Image("/com/dansoftware/libraryapp/image/util/google_12px.png")),
+                Label(I18N.getGoogleBooksImportValue("google.books.tile.title")).also {
+                    it.styleClass.add("tile-title")
+                })
 
         private fun buildVBox(volume: Volume): VBox =
             VBox(3.0).also {
+                setVgrow(it, Priority.ALWAYS)
+                it.children.add(buildTitle(volume))
+                it.children.add(buildSubtitle(volume))
+                it.children.add(buildAuthorsLabel(volume))
+            }
 
+        private fun buildTitle(volume: Volume) =
+            SelectableLabel(volume.volumeInfo?.title).also {
+                it.styleClass.add("title-label")
+            }
+
+        private fun buildSubtitle(volume: Volume) =
+            SelectableLabel(volume.volumeInfo?.subtitle).also {
+                it.styleClass.add("sub-title-label")
+            }
+
+        private fun buildAuthorsLabel(volume: Volume) =
+            SelectableLabel(volume.volumeInfo?.authors?.joinToString(", ")).also {
+                it.styleClass.add("authors-label")
             }
     }
 }
