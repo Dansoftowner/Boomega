917390a73871fc660191ddaab64d5082e5bde236
Daniel Gyorffy <judalion777@gmail.com>
2021-01-28 14:24:08 +0100

    :construction: Improving GoogleBookDockNode

--- src/main/java/com/dansoftware/libraryapp/gui/googlebooks/dock/GoogleBookDockNode.java
+++ src/main/java/com/dansoftware/libraryapp/gui/googlebooks/dock/GoogleBookDockNode.java
@@ -5,26 +5,35 @@ import com.dansoftware.dock.docksystem.DockSystem;
 import com.dansoftware.libraryapp.googlebooks.Volume;
 import com.dansoftware.libraryapp.gui.context.Context;
 import com.dansoftware.libraryapp.gui.googlebooks.SearchParameters;
+import com.dansoftware.libraryapp.i18n.I18N;
 import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class GoogleBookDockNode<T> extends DockNode {
 
+    private final GoogleBookDockContent<T> content;
+
     public GoogleBookDockNode(@NotNull DockSystem<?> dockSystem,
                               @NotNull Context context,
-                              @NotNull ObservableList<T> items,
+                              @Nullable List<T> items,
                               @NotNull BiConsumer<T, Volume> joinAction,
                               @NotNull BiConsumer<T, Volume> removeAction,
                               @NotNull Function<T, SearchParameters> searchParametersSupplier,
                               @NotNull Function<T, String> volumeHandleRetriever) {
-        super(dockSystem, new ImageView(new Image("/com/dansoftware/libraryapp/image/util/google_12px.png")),
-                "Google Books", //TODO: i18n
-                new GoogleBookDockContent<T>(context, items, joinAction, removeAction, searchParametersSupplier, volumeHandleRetriever)
-        );
+        super(dockSystem, I18N.getGoogleBooksImportValue("google.books.dock.title"),
+                new ImageView(new Image("/com/dansoftware/libraryapp/image/util/google_12px.png")));
+        this.content = new GoogleBookDockContent<T>(context, items, joinAction, removeAction, searchParametersSupplier, volumeHandleRetriever);
+        setContent(content);
+    }
+
+    public void setItems(@Nullable List<T> items) {
+        this.content.setItems(items);
     }
 }
