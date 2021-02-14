a3a69b6ddf11c0bbb237fc68eea06a30a492b671
Daniel Gyorffy <judalion777@gmail.com>
2021-02-14 18:49:45 +0100

    :sparkles: Dock rearrangement in RecordsView

--- src/main/java/com/dansoftware/libraryapp/gui/record/show/RecordsView.java
+++ src/main/java/com/dansoftware/libraryapp/gui/record/show/RecordsView.java
@@ -8,9 +8,15 @@ import com.dansoftware.libraryapp.gui.record.show.dock.googlebook.GoogleBookConn
 import com.dansoftware.libraryapp.i18n.I18N;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.beans.binding.Bindings;
 import javafx.collections.ListChangeListener;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
@@ -45,8 +51,8 @@ public class RecordsView extends SplitPane {
         splitPane.setOrientation(Orientation.VERTICAL);
         splitPane.setPrefWidth(500);
         splitPane.setMaxWidth(500);
-        splitPane.getItems().add(buildBookEditorDock());
-        splitPane.getItems().add(buildGoogleBooksDock());
+        splitPane.getItems().add(buildBookEditorDock(splitPane));
+        splitPane.getItems().add(buildGoogleBooksDock(splitPane));
         SplitPane.setResizableWithParent(splitPane, false);
         return splitPane;
     }
@@ -59,23 +65,23 @@ public class RecordsView extends SplitPane {
         this.getItems().addAll(recordTable, dockSplitPane);
     }
 
-    private Node buildBookEditorDock() {
+    private Node buildBookEditorDock(SplitPane dockSplitPane) {
         var recordEditor = new RecordEditor(context, database, this.recordTable.getSelectionModel().getSelectedItems());
         recordEditor.setOnItemsDeleted(items -> recordTable.getItems().removeAll(items));
         recordEditor.setOnItemsModified(items -> recordTable.refresh());
         this.recordTable.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super Record>) change -> {
             recordEditor.setItems(this.recordTable.getSelectionModel().getSelectedItems());
         });
-        return new RecordEditorDock(recordEditor);
+        return new RecordEditorDock(dockSplitPane, recordEditor);
     }
 
-    private Node buildGoogleBooksDock() {
+    private Node buildGoogleBooksDock(SplitPane dockSplitPane) {
         var dockContent = new GoogleBookConnectionView(context, database, recordTable.getSelectionModel().getSelectedItems());
         dockContent.setOnRefreshed(recordTable::refresh);
         this.recordTable.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super Record>) change -> {
             dockContent.setItems(this.recordTable.getSelectionModel().getSelectedItems());
         });
-        return new GoogleBookDock(dockContent);
+        return new GoogleBookDock(dockSplitPane, dockContent);
     }
 
     public void setDockFullyResizable() {
@@ -102,26 +108,61 @@ public class RecordsView extends SplitPane {
 
         private final T content;
 
-        TitledDock(@NotNull Node icon,
+        TitledDock(@NotNull SplitPane parent,
+                   @NotNull Node icon,
                    @NotNull String title,
                    @NotNull T content) {
             this.content = content;
-            this.buildUI(icon, title);
+            this.buildUI(parent, icon, title);
         }
 
-        private void buildUI(Node icon, String title) {
+        private void buildUI(SplitPane parent, Node icon, String title) {
             SplitPane.setResizableWithParent(this, false);
             VBox.setVgrow(content, Priority.ALWAYS);
             getStyleClass().add("titled-dock");
-            getChildren().add(buildTitleBar(icon, title));
+            getChildren().add(buildTitleBar(parent, icon, title));
             getChildren().add(content);
         }
 
-        private Node buildTitleBar(Node icon, String title) {
-            var hBox = new HBox(10.0, new StackPane(icon), new Label(title));
+        private Node buildTitleBar(SplitPane parent, Node icon, String title) {
+            var buttonBar = new HBox(5.0, buildUpButton(parent), buildDownButton(parent));
+            var iconTitleBar = new HBox(10.0, new StackPane(icon), new StackPane(new Label(title)));
+            var titleBar = new BorderPane(null, null, buttonBar, null, iconTitleBar);
+            titleBar.getStyleClass().add("dock-title-bar");
+            return titleBar;
+        }
 
-            hBox.getStyleClass().add("dock-title-bar");
-            return hBox;
+        private Button buildUpButton(SplitPane parent) {
+            Button btn = buildRearrangeButton(MaterialDesignIcon.ARROW_UP);
+            parent.getItems().addListener((ListChangeListener<? super Node>) modification -> {
+                btn.setDisable(parent.getItems().size() < 2 || parent.getItems().indexOf(this) == 0);
+            });
+            btn.setOnAction(e -> {
+                int index = parent.getItems().indexOf(this);
+                parent.getItems().remove(this);
+                parent.getItems().add(index -1, this);
+            });
+            return btn;
+        }
+
+        private Button buildDownButton(SplitPane parent) {
+            Button btn = buildRearrangeButton(MaterialDesignIcon.ARROW_DOWN);
+            parent.getItems().addListener((ListChangeListener<? super Node>) modification -> {
+                btn.setDisable(parent.getItems().size() < 2 || parent.getItems().indexOf(this) == parent.getItems().size() -1);
+            });
+            btn.setOnAction(e -> {
+                int index = parent.getItems().indexOf(this);
+                parent.getItems().remove(this);
+                parent.getItems().add(index + 1, this);
+            });
+            return btn;
+        }
+
+        private Button buildRearrangeButton(MaterialDesignIcon icon) {
+            var btn = new Button(null, new MaterialDesignIconView(icon));
+            btn.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            btn.setPadding(new Insets(0));
+            return btn;
         }
     }
 
@@ -129,18 +170,27 @@ public class RecordsView extends SplitPane {
 
         private final GoogleBookConnectionView content;
 
-        GoogleBookDock(@NotNull GoogleBookConnectionView content) {
-            super(new ImageView(new Image("/com/dansoftware/libraryapp/image/util/google_12px.png")),
+        GoogleBookDock(@NotNull SplitPane parent, @NotNull GoogleBookConnectionView content) {
+            super(
+                    parent,
+                    new ImageView(new Image("/com/dansoftware/libraryapp/image/util/google_12px.png")),
                     I18N.getValue("google.books.dock.title"),
-                    content);
+                    content
+            );
             this.content = content;
         }
     }
 
     private static final class RecordEditorDock extends TitledDock<RecordEditor> {
 
-        RecordEditorDock(@NotNull RecordEditor content) {
-            super(new FontAwesomeIconView(FontAwesomeIcon.EDIT), I18N.getValue("record.editor.dock.title"), content);
+        RecordEditorDock(@NotNull SplitPane parent,
+                         @NotNull RecordEditor content) {
+            super(
+                    parent,
+                    new FontAwesomeIconView(FontAwesomeIcon.EDIT),
+                    I18N.getValue("record.editor.dock.title"),
+                    content
+            );
         }
     }
 
