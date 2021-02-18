0cdb277c3caaf10284d8af20619195ae1ad9f4b6
Daniel Gyorffy <judalion777@gmail.com>
2021-02-18 16:06:01 +0100

    :boom: Changing default properties: plugin directories and refactoring constant names

--- src/main/java/com/dansoftware/libraryapp/main/PropertiesResponsible.kt
+++ src/main/java/com/dansoftware/libraryapp/main/PropertiesResponsible.kt
@@ -16,9 +16,8 @@ object PropertiesResponsible {
 
     /* ****** Keys ****** */
 
-    private const val LIBRARY_APP_VERSION = "libraryapp.version"
-    private const val LIBRARY_APP_BUILD_INFO = "libraryapp.build.info"
-    private const val LIBRARY_APP_FILE_EXTENSION = "libraryapp.file.extension"
+    private const val BOOMEGA_VERSION = "libraryapp.version"
+    private const val BOOMEGA_FILE_EXTENSION = "libraryapp.file.extension"
     private const val LOG_FILE_PATH = "log.file.path"
     private const val LOG_FILE_FULL_PATH = "log.file.path.full"
     private const val PLUGIN_DIRECTORY_PATH = "libraryapp.plugin.dir"
@@ -30,17 +29,12 @@ object PropertiesResponsible {
     /**
      * the app's version
      */
-    private const val LIBRARY_APP_VERSION_VALUE = "0.0.0"
-
-    /**
-     * some info of this build
-     */
-    private const val LIBRARY_APP_BUILD_INFO_VALUE = "Built on 2020-09-18"
+    private const val BOOMEGA_VERSION_VALUE = "0.0.0"
 
     /**
      * the libraryapp database file-extension
      */
-    private const val LIBRARY_APP_FILE_EXTENSION_VALUE = "bmdb"
+    private const val BOOMEGA_FILE_EXTENSION_VALUE = "bmdb"
 
     /**
      * The name of the default Boomega documents folder
@@ -85,9 +79,8 @@ object PropertiesResponsible {
      * Puts the libraryapp-specific values into the system properties
      */
     private fun putAppSpecificProperties() {
-        System.setProperty(LIBRARY_APP_VERSION, LIBRARY_APP_VERSION_VALUE)
-        System.setProperty(LIBRARY_APP_BUILD_INFO, LIBRARY_APP_BUILD_INFO_VALUE)
-        System.setProperty(LIBRARY_APP_FILE_EXTENSION, LIBRARY_APP_FILE_EXTENSION_VALUE)
+        System.setProperty(BOOMEGA_VERSION, BOOMEGA_VERSION_VALUE)
+        System.setProperty(BOOMEGA_FILE_EXTENSION, BOOMEGA_FILE_EXTENSION_VALUE)
         System.setProperty(PLUGIN_DIRECTORY_PATH, getPluginDirPath())
         System.setProperty(CONFIG_FILE_PATH, getConfigFilePath())
         System.setProperty(DEFAULT_DIRECTORY_PATH, getDefaultDirectoryFilePath());
@@ -107,26 +100,23 @@ object PropertiesResponsible {
      */
     private fun getPluginDirPath(): String = when {
         OsInfo.isWindows() -> {
-            var rootDir: String = System.getenv("APPDATA")
-            if (StringUtils.isBlank(rootDir)) {
-                rootDir = FileUtils.getUserDirectoryPath()
+            var appdata: String = System.getenv("APPDATA")
+            if (StringUtils.isBlank(appdata)) {
+                appdata = FileUtils.getUserDirectoryPath()
             }
             File(
-                File(rootDir),
+                File(appdata),
                 listOf("Dansoftware", "boomega", "plugin").joinToString(File.separator)
             ).absolutePath
         }
         OsInfo.isLinux() ->
             listOf(
                 FileUtils.getUserDirectoryPath(),
-                ".libraryapp2020",
-                "profiles",
+                "boomega",
                 "plugin"
             ).joinToString(File.separator)
         OsInfo.isMac() ->
-            listOf("~", "Library", "Application", "Support", "Dansoftware", "libraryapp2020", "plugin").joinToString(
-                File.separator
-            )
+            listOf(FileUtils.getUserDirectoryPath(), "boomega", "plugin").joinToString(File.separator)
         else -> "plugin"
     }
 }
