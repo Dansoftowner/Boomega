cc79a33bc56abde04fb4806a217d15151997cb52
Daniel Gyorffy <judalion777@gmail.com>
2021-02-22 12:10:34 +0100

    :sparkles: Home button look for module-add-btn in MainContentView

--- src/main/java/com/dansoftware/boomega/gui/mainview/MainContentView.java
+++ src/main/java/com/dansoftware/boomega/gui/mainview/MainContentView.java
@@ -8,6 +8,13 @@ import com.dansoftware.boomega.gui.googlebooks.GoogleBooksImportModule;
 import com.dansoftware.boomega.gui.record.add.RecordAddModule;
 import com.dansoftware.boomega.gui.record.show.RecordsViewModule;
 import com.dlsc.workbenchfx.Workbench;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Window;
 import org.jetbrains.annotations.NotNull;
 
 public class MainContentView extends Workbench implements ContextTransformable {
@@ -21,6 +28,40 @@ public class MainContentView extends Workbench implements ContextTransformable {
         this.preferences = preferences;
         this.database = database;
         initModules();
+        initUiModifications();
+    }
+
+    private void initUiModifications() {
+        this.sceneProperty().addListener(new ChangeListener<Scene>() {
+            @Override
+            public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene scene) {
+                if (scene != null) {
+                    scene.windowProperty().addListener(new ChangeListener<Window>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Window> observable, Window oldValue, Window window) {
+                            if (window != null) {
+                                window.showingProperty().addListener(new ChangeListener<Boolean>() {
+                                    @Override
+                                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean windowShown) {
+                                        if (windowShown) {
+                                            modifyAddButton();
+                                            observable.removeListener(this);
+                                        }
+                                    }
+                                });
+                                observable.removeListener(this);
+                            }
+                        }
+                    });
+                    observable.removeListener(this);
+                }
+            }
+        });
+    }
+
+    private void modifyAddButton() {
+        final Button addButton = (Button) MainContentView.this.lookup("#add-button");
+        addButton.setGraphic(new MaterialDesignIconView(MaterialDesignIcon.HOME));
     }
 
     private void initModules() {
