6604ff17a061780fedf309194390e59d5bc8a303
Daniel Gyorffy <judalion777@gmail.com>
2021-03-01 11:26:30 +0100

    :adhesive_bandage: Removing resize policy from notes-editor in RecordAddView

--- src/main/java/com/dansoftware/boomega/gui/record/add/FormNotesEditor.kt
+++ src/main/java/com/dansoftware/boomega/gui/record/add/FormNotesEditor.kt
@@ -2,20 +2,13 @@ package com.dansoftware.boomega.gui.record.add
 
 import com.dansoftware.boomega.i18n.I18N
 import javafx.geometry.Insets
-import javafx.scene.Cursor
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Priority
-import javafx.scene.layout.StackPane
 import javafx.scene.web.HTMLEditor
 
 private class FormNotesEditor : BorderPane() {
-
-    companion object {
-        const val PREFERRED_RESIZE_SPEED = 2.0
-        const val PREFERRED_RESIZE_AREA_SIZE = 3.5
-    }
 
     private val htmlEditor: HTMLEditor = HTMLEditor()
 
@@ -27,7 +20,6 @@ private class FormNotesEditor : BorderPane() {
 
     init {
         this.buildBase()
-        this.buildResizePolicy()
     }
 
     private fun buildBase() {
@@ -40,25 +32,5 @@ private class FormNotesEditor : BorderPane() {
         padding = Insets(5.0)
         children.add(Label(I18N.getValue("record.add.form.notes")))
         styleClass.add("notes-title-bar")
-    }
-
-    @Suppress("DuplicatedCode")
-    private fun buildResizePolicy() {
-        StackPane().let {
-            bottom = it
-            it.prefHeight = PREFERRED_RESIZE_AREA_SIZE
-            it.cursor = Cursor.V_RESIZE
-            var released = false
-            var lastY: Double? = null
-            it.setOnMouseReleased { released = true }
-            it.setOnMouseDragged { event ->
-                if (lastY === null || released) {
-                    lastY = event.sceneY
-                    released = false
-                }
-                this.prefHeight = this.height + (event.sceneY - lastY!!) * PREFERRED_RESIZE_SPEED
-                lastY = event.sceneY
-            }
-        }
     }
 }
