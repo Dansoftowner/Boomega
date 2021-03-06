ff1226ac205b0fb676352f7001e417d1137084f6
Daniel Gyorffy <judalion777@gmail.com>
2021-03-06 15:17:06 +0100

    :art: Deprecating UIUtils

--- src/main/java/com/dansoftware/boomega/gui/util/UIUtils.java
+++ src/main/java/com/dansoftware/boomega/gui/util/UIUtils.java
@@ -4,16 +4,25 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
+@Deprecated
 public class UIUtils {
 
     private UIUtils() {
     }
 
+    /**
+     * @deprecated use {@link BaseFXUtils#runOnUiThread(Runnable)} instead
+     */
+    @Deprecated
     public static void runOnUiThread(Runnable action) {
         if (Platform.isFxApplicationThread()) action.run();
         else Platform.runLater(action);
     }
 
+    /**
+     * @deprecated use {@link BaseFXUtils#refresh(ComboBox)} instead
+     */
+    @Deprecated
     public static <T> void refreshComboBox(ComboBox<T> comboBox) {
         ObservableList<T> items = comboBox.getItems();
         T selected = comboBox.getSelectionModel().getSelectedItem();
