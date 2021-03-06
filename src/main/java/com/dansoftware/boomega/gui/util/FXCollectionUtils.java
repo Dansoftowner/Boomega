f42b65a0655d1d1f4be66c598da444688ae22d25
Daniel Gyorffy <judalion777@gmail.com>
2021-03-06 15:21:36 +0100

    :art: Deprecating FXCollectionUtils

--- src/main/java/com/dansoftware/boomega/gui/util/FXCollectionUtils.java
+++ src/main/java/com/dansoftware/boomega/gui/util/FXCollectionUtils.java
@@ -5,11 +5,16 @@ import javafx.collections.ObservableList;
 
 import java.util.List;
 
+@Deprecated
 public class FXCollectionUtils {
 
     private FXCollectionUtils() {
     }
 
+    /**
+     * @deprecated use {@link BaseFXUtils#copy(ObservableList)} instead
+     */
+    @Deprecated
     public static <T> ObservableList<T> copyOf(ObservableList<T> observableList) {
         return FXCollections.observableArrayList(List.copyOf(observableList));
     }
