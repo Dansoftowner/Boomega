dc091030a19a749455f8f44c7a861dd1e9982ce0
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2021-04-14 10:13:37 +0200

    :page_facing_up: Some license headers

--- src/main/java/com/dansoftware/boomega/config/Preferences.java
+++ src/main/java/com/dansoftware/boomega/config/Preferences.java
@@ -1,3 +1,21 @@
+/*
+ * Boomega
+ * Copyright (C)  2021  Daniel Gyoerffy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.dansoftware.boomega.config;
 
 import com.dansoftware.boomega.config.source.ConfigSource;
@@ -23,7 +41,6 @@ import java.util.function.Consumer;
  * For modifying the data we can use an {@link Editor} object that can be instantiated by the {@link #editor()} method.
  *
  * @author Daniel Gyorffy
- * @see ConfigFile
  */
 public class Preferences {
 
