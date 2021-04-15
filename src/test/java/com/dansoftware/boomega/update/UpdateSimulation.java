5fdd2a00096bdeffd9cf537374888218b6381bee
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2021-04-15 13:22:00 +0200

    :art: Updating update-searching simulation

--- src/test/java/com/dansoftware/boomega/update/UpdateSimulation.java
+++ src/test/java/com/dansoftware/boomega/update/UpdateSimulation.java
@@ -1,17 +1,16 @@
 package com.dansoftware.boomega.update;
 
+import com.dansoftware.boomega.config.Preferences;
 import com.dansoftware.boomega.config.source.InMemorySource;
 import com.dansoftware.boomega.main.Main;
 import com.dansoftware.boomega.main.PropertiesResponsible;
 import com.dansoftware.boomega.util.adapter.VersionInteger;
-import com.dansoftware.boomega.config.Preferences;
-import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -68,7 +67,7 @@ public class UpdateSimulation {
                 @NotNull
                 @Override
                 public Reader reviewReader() {
-                    return new StringReader(previewText()) {
+                    return new InputStreamReader(getClass().getResourceAsStream("/com/dansoftware/boomega/update/sample-review.md"), StandardCharsets.UTF_8) {
                         @Override
                         public int read(char[] cbuf, int off, int len) throws IOException {
                             sleep(1_000);
@@ -79,16 +78,12 @@ public class UpdateSimulation {
             };
         }
 
-        private String previewText() {
-            try {
-                return IOUtils.toString(getClass().getResource("/com/dansoftware/boomega/update/sample-review.md"), StandardCharsets.UTF_8);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
         private List<DownloadableBinary> buildFakeBinaries() {
-            return List.of(new FakeBinary("Exe installer", "exe", 112));
+            return List.of(
+                    new FakeBinary("Exe installer", "exe", 112),
+                    new FakeBinary("Zip archive", "zip", 140),
+                    new FakeBinary("Jar archive", "jar", 40)
+            );
         }
     }
 
