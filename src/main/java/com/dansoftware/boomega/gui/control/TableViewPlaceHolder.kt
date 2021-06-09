4f7d0bb9a51970b982f554f447de3880060ccb73
Daniel Gyorffy <judalion777@gmail.com>
2021-06-09 11:03:44 +0200

    :art: Table view place holder improvements

--- src/main/java/com/dansoftware/boomega/gui/control/TableViewPlaceHolder.kt
+++ src/main/java/com/dansoftware/boomega/gui/control/TableViewPlaceHolder.kt
@@ -3,12 +3,15 @@ package com.dansoftware.boomega.gui.control
 import javafx.beans.binding.Bindings
 import javafx.beans.binding.BooleanBinding
 import javafx.scene.Group
+import javafx.scene.Node
 import javafx.scene.control.Label
 import javafx.scene.control.TableView
 import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
+import java.util.function.Consumer
+import java.util.function.Supplier
 
-class TableViewPlaceHolder(tableView: TableView<*>, valueIfEmpty: () -> String, valueIfNoColumns: () -> String) :
+class TableViewPlaceHolder(tableView: TableView<*>, valueIfEmpty: Supplier<Node>, valueIfNoColumns: Supplier<Node>) :
     StackPane() {
 
     @Suppress("JoinDeclarationAndAssignment")
@@ -19,17 +22,18 @@ class TableViewPlaceHolder(tableView: TableView<*>, valueIfEmpty: () -> String, 
         this.buildUI(valueIfEmpty, valueIfNoColumns)
     }
 
-    private fun buildUI(valueIfEmpty: () -> String, valueIfNoColumns: () -> String) {
-        children.add(Group(VBox(
-            // TODO: GRAPHIC,
-            Label().also {
-                noColumns.addListener { _, _, noColumns: Boolean ->
-                    it.text = when {
-                        noColumns -> valueIfNoColumns()
-                        else -> valueIfEmpty()
-                    }
-                }
+    constructor(tableView: TableView<*>, valueIfEmpty: () -> String, valueIfNoColumns: () -> String) : this(
+        tableView,
+        Supplier { Label(valueIfEmpty()) },
+        Supplier { Label(valueIfNoColumns()) }
+    )
+
+    private fun buildUI(valueIfEmpty: Supplier<Node>, valueIfNoColumns: Supplier<Node>) {
+        noColumns.addListener { _, _, noColumns ->
+            when {
+                noColumns -> children.setAll(Group(valueIfNoColumns.get()))
+                else -> children.setAll(Group(valueIfEmpty.get()))
             }
-        )))
+        }
     }
 }
