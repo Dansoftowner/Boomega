2973992fd4e4c8c2173dc574ea0b799298fd61e4
Daniel Gyorffy <judalion777@gmail.com>
2021-12-01 13:27:22 +0100

    :construction: New Mysql registration form structure

--- src/main/java/com/dansoftware/boomega/gui/database/mysql/MySQLRegistrationForm.kt
+++ src/main/java/com/dansoftware/boomega/gui/database/mysql/MySQLRegistrationForm.kt
@@ -24,6 +24,7 @@ import com.dansoftware.boomega.database.mysql.MySQLMeta
 import com.dansoftware.boomega.database.mysql.MySQLVersion
 import com.dansoftware.boomega.gui.api.Context
 import com.dansoftware.boomega.gui.util.selectedItemProperty
+import com.dansoftware.boomega.i18n.i18n
 import javafx.beans.binding.Bindings
 import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
@@ -31,8 +32,8 @@ import javafx.scene.Node
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
-import javafx.scene.layout.HBox
-import javafx.scene.layout.VBox
+import javafx.scene.layout.GridPane
+import javafx.scene.layout.Priority
 
 class MySQLRegistrationForm(
     context: Context,
@@ -45,13 +46,13 @@ class MySQLRegistrationForm(
     private val version = SimpleObjectProperty<MySQLVersion>()
 
     override val node: Node
-        get() = VBox(
+        get() = Grid()/* VBox(
             5.0,
             HBox(5.0, Label("Host:"), buildHostField()),
             HBox(5.0, Label("Port:"), buildPortField()),
             HBox(5.0, Label("Database name:"), buildDatabaseNameField()),
             HBox(5.0, Label("Version:"), buildVersionChooser())
-        )
+        )*/
 
     private fun buildHostField() = TextField().apply {
         // TODO: prompt text
@@ -73,6 +74,56 @@ class MySQLRegistrationForm(
         items.addAll(MySQLVersion.values())
         selectionModel.select(0)
         version.bind(selectedItemProperty())
+    }
+
+    private inner class Grid : GridPane() {
+        init {
+            buildUI()
+        }
+
+        private fun buildUI() {
+            // TODO: i18n
+            children.add(buildLabel("Host:", 0, 0))
+            children.add(buildHostField())
+            children.add(buildLabel("Port:", 1, 0))
+            children.add(buildPortField())
+            children.add(buildLabel("Name:", 0, 1))
+            children.add(buildNameField())
+        }
+
+        private fun buildLabel(i18n: String, column: Int, row: Int) = Label(i18n(i18n)).apply {
+            setConstraints(this, column, row)
+        }
+
+        private fun buildHostField() = TextField().apply {
+            // TODO: prompt text
+            // TODO: validation
+            setConstraints(this, 0, 1)
+            setHgrow(this, Priority.SOMETIMES)
+            Bindings.bindBidirectional(host, textProperty())
+            minHeight = 35.0
+        }
+
+        private fun buildPortField() = TextField().apply {
+            // TODO: prompt text
+            // TODO: validation
+            setConstraints(this, 1, 1)
+            setHgrow(this, Priority.SOMETIMES)
+            Bindings.bindBidirectional(port, textProperty())
+            minHeight = 35.0
+        }
+
+        private fun buildNameField() = TextField().apply {
+            // TODO: prompt text
+            // TODO: validation
+            setConstraints(this, 1, 1)
+            setHgrow(this, Priority.SOMETIMES)
+            Bindings.bindBidirectional(databaseName, textProperty())
+            minHeight = 35.0
+        }
+
+
+
     }
 
     override fun registrate(): MySQLMeta {
