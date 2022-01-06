41bc9e4e3fb984be1edf15e07b9686a29b5ecca1
Daniel Gyorffy <judalion777@gmail.com>
2022-01-06 10:16:11 +0100

    :sparkles: Native info view

--- src/main/java/com/dansoftware/boomega/gui/info/native/NativeInfoView.kt
+++ src/main/java/com/dansoftware/boomega/gui/info/native/NativeInfoView.kt
@@ -21,6 +21,7 @@ package com.dansoftware.boomega.gui.info.native
 import com.dansoftware.boomega.gui.control.HighlightableLabel
 import com.dansoftware.boomega.gui.util.addRow
 import com.dansoftware.boomega.gui.util.colspan
+import com.dansoftware.boomega.util.byteCountToDisplaySize
 import javafx.geometry.Insets
 import javafx.scene.control.Label
 import javafx.scene.control.Separator
@@ -30,10 +31,16 @@ import oshi.software.os.OperatingSystem
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * Gives an overview of the basic information about the process and the os.
+ * > Note: it's not internationalized because it's not important for average users.
+ */
 class NativeInfoView : GridPane() {
 
     private val systemInfo = SystemInfo()
+    private val os = systemInfo.operatingSystem
     private val OperatingSystem.currentProcess get() = getProcess(processId)
+    private val currentProcess = systemInfo.operatingSystem.currentProcess
 
     init {
         padding = Insets(10.0)
@@ -41,70 +48,24 @@ class NativeInfoView : GridPane() {
     }
 
     private fun buildUI() {
-        addRow(Label("PID: "), buildPIDLabel())
-        addRow(Label("UID: "), buildUIDLabel())
-        addRow(Label("PRI: "), buildPRILabel())
-        addRow(Label("(Owner)Username: "), buildUsernameLabel())
-        addRow(Label("Elevated: "), buildElevatedLabel())
-        addRow(Label("Priority: "), buildPriorityLabel())
-        addRow(Label("Start time: "), buildStartTimeLabel())
-        addRow(Label("Command line: "), buildCommandLabel())
+        addRow(Label("PID: "), label(currentProcess.processID))
+        addRow(Label("UID: "), label(currentProcess.userID))
+        addRow(Label("PRI: "), label(currentProcess.parentProcessID))
+        addRow(Label("(Owner)Username: "), label(currentProcess.user))
+        addRow(Label("Elevated: "), label(os.isElevated))
+        addRow(Label("Priority: "), label(currentProcess.priority))
+        addRow(Label("Start time: "), label(currentProcess.startTime.formatMillisToDate("yyyy-MM-DD HH:mm")))
+        addRow(Label("Bytes read: "), label(byteCountToDisplaySize(currentProcess.bytesRead)))
+        addRow(Label("Bytes written: "), label(byteCountToDisplaySize(currentProcess.bytesWritten)))
+        addRow(Label("Command line: "), label(currentProcess.commandLine))
         addRow(Separator().colspan(2))
-        addRow(Label("OS Manufacturer: "), buildOSManufacturerLabel())
-        addRow(Label("Bitness: "), buildBitnessLabel())
+        addRow(Label("OS Name: "), label(os.family))
+        addRow(Label("OS Manufacturer: "), label(os.manufacturer))
+        addRow(Label("OS Version: "), label(os.versionInfo.version))
+        addRow(Label("OS build number: "), label(os.versionInfo.buildNumber))
+        addRow(Label("Bitness: "), label(os.bitness))
     }
 
-    private fun buildPIDLabel() = HighlightableLabel().apply {
-        // TODO: clean this
-        text = systemInfo.operatingSystem.processId.toString()
-    }
-
-    private fun buildUIDLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.userID
-    }
-
-    private fun buildPRILabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.parentProcessID.toString()
-    }
-
-    private fun buildElevatedLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.isElevated.toString()
-    }
-
-    private fun buildPriorityLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.priority.toString()
-    }
-
-    private fun buildStartTimeLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.startTime.formatMillisToDate("yyyy-MM-DD HH:mm")
-    }
-
-    private fun buildBytesReadLabel() = HighlightableLabel().apply {
-        //text = systemInfo.operatingSystem.currentProcess
-    }
-
-    private fun buildCommandLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.commandLine
-    }
-
-    private fun buildUsernameLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.currentProcess.user
-    }
-
-    private fun buildOSManufacturerLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.manufacturer.toString()
-    }
-
-    private fun buildBitnessLabel() = HighlightableLabel().apply {
-        text = systemInfo.operatingSystem.bitness.toString()
-    }
-
-    private fun buildFileSystemLabel() = Label().apply {
-//        text = systemInfo.operatingSystem.
-    }
-
-    private fun Long.formatMillisToDate(format: String): String {
-        return SimpleDateFormat(format).format(Date(this))
-    }
-
+    private fun label(value: Any?) = HighlightableLabel(value.toString())
+    private fun Long.formatMillisToDate(format: String): String = SimpleDateFormat(format).format(Date(this))
 }
