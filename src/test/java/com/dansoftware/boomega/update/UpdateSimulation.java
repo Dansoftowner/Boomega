e1f30b90c7706693181e9700b10929fd6ee2fd02
Daniel Gyorffy <judalion777@gmail.com>
2022-03-03 10:58:48 +0100

    :art: refactoring in test code

--- src/test/java/com/dansoftware/boomega/update/UpdateSimulation.java
+++ src/test/java/com/dansoftware/boomega/update/UpdateSimulation.java
@@ -1,7 +1,25 @@
+/*
+ * Boomega
+ * Copyright (c) 2020-2022  Daniel Gyoerffy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.dansoftware.boomega.update;
 
+import com.dansoftware.boomega.config.DummyConfigSource;
 import com.dansoftware.boomega.config.source.ConfigSource;
-import com.dansoftware.boomega.config.source.InMemorySource;
 import com.dansoftware.boomega.di.DIService;
 import com.dansoftware.boomega.gui.app.BaseBoomegaApplication;
 import com.dansoftware.boomega.gui.app.BoomegaApp;
@@ -34,13 +52,6 @@ public class UpdateSimulation {
 
     public static void main(String[] args) {
         BaseBoomegaApplication.launchApp(BoomegaApp.class);
-    }
-
-    private static class DummyConfigSource extends InMemorySource {
-        @Override
-        public boolean isCreated() {
-            return false;
-        }
     }
 
     private static class DummyReleasesFetcher implements ReleasesFetcher {
